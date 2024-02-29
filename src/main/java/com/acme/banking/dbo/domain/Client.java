3c4790241770af82c513ec9b1e1528c24c2f1413
asdaroot <129074366+asda101@users.noreply.github.com>
2024-02-29 11:01:44 +0300

    Add getAccount collection and addAccount method to Client class V1

--- src/main/java/com/acme/banking/dbo/domain/Client.java
+++ src/main/java/com/acme/banking/dbo/domain/Client.java
@@ -10,10 +10,10 @@ public class Client {
 
     public Client(int id, String name) {
         if (id < 0) {
-            throw new IllegalArgumentException("ID must be a positive integer or 0");
+            throw new IllegalArgumentException("ID must be a positive integer or 0.");
         }
         if (name == null || name.trim().isEmpty()) {
-            throw new IllegalArgumentException("Name must not be null or empty");
+            throw new IllegalArgumentException("Name must not be null or empty.");
         }
 
         this.id = id;
@@ -26,5 +26,17 @@ public class Client {
 
     public String getName() {
         return name;
+    }
+
+    public Collection<Account> getAccounts() {
+        return this.accounts;
+    }
+
+    public void addAccount(Account newAccount) {
+        if (newAccount == null) throw new IllegalArgumentException("Новый счет не должен быть null.");
+        if (newAccount.getClient().id != this.id) throw new IllegalArgumentException("Новый счет не принадлежит этому клиенту.");
+        if (this.accounts.contains(newAccount)) throw new IllegalArgumentException("Счет уже существует для этого клиента.");
+
+        accounts.add(newAccount);
     }
 }
